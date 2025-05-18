849f9261a6ee8ef897f0ea5daf4228f7625b3ba4
Allieniaszxy <89945513+Allieniaszxy@users.noreply.github.com>
2025-05-18 20:50:34 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,47 +1,12 @@
-let mystr = "Hello, Javascript World!";
+let name = [
+    "ope", "dami", "gbemisola"{
+        score: 50,
+        age: 12,
+    }
+]
+console.log(name);
 
-console.log(mystr);
-console.log(mystr.toUpperCase);
-console.log(mystr.toLowerCase);
-console.log(mystr.charAt());
-console.log(mystr.indexOf("J"));
-console.log(mystr.includes("bob"));
-console.log(mystr.replace("Hello", "Welcome to"));
-console.log(mystr.split(" "));
-console.log(mystr.startsWith("hi"));
-console.log(mystr.endsWith("good"));
-
-// number method
-let num= 42.678;
-let strnum = "89.56";
-
-console.log(num);
-console.log(num.toString());
-console.log(num.toFixed(2));
-console.log(parseInt(strnum));
-console.log(parseFloat(strnum));
-console.log(isNaN("trhhf"));
-
-
-// math method
-console.log(Math.PI);
-console.log(Math.PI.toFixed(2));
-console.log(Math.pow(5,2));
-console.log(Math.abs(-2));
-console.log(Math.sqrt(12));
-console.log(Math.floor(24.54))
-
-
-// functions
-function name(name){
-    console.log(name)
-}
-
-name("ay");
-name("charles");
-
-function addNum(a,b){
-    console.log(a+b)
-}
-
-addNum(13, 12)
+let city = [
+    "ota", "ebute",
+]
+console.log(city)
